a3d9afa098b52473d8995921f7769242fd60b7a5
vsidtiladsha <113388330+vsidtiladsha@users.noreply.github.com>
2022-10-24 14:24:56 -0700

    working on explosion

--- main.ts
+++ main.ts
@@ -48,9 +48,56 @@ function makeBoatVisible (boatArray: Sprite[]) {
         previousBoatSprite.setFlag(SpriteFlag.Invisible, false)
     }
 }
+function ishitOrmiss (enemyBoats: Sprite[][]) {
+    for (let index = 0; index <= 2; index++) {
+        for (let currentBoatSprite2 of enemyBoats[index]) {
+            if (grid.spriteCol(currentBoatSprite2) == grid.spriteCol(cursor) && grid.spriteRow(currentBoatSprite2) == grid.spriteRow(cursor)) {
+                boomsprite = sprites.create(img`
+                    . . . . 2 2 2 2 2 2 2 2 . . . . 
+                    . . . 2 4 4 4 5 5 4 4 4 2 2 2 . 
+                    . 2 2 5 5 d 4 5 5 5 4 4 4 4 2 . 
+                    . 2 4 5 5 5 5 d 5 5 5 4 5 4 2 2 
+                    . 2 4 d d 5 5 5 5 5 5 d 4 4 4 2 
+                    2 4 5 5 d 5 5 5 d d d 5 5 5 4 4 
+                    2 4 5 5 4 4 4 d 5 5 d 5 5 5 4 4 
+                    4 4 4 4 . . 2 4 5 5 . . 4 4 4 4 
+                    . . b b b b 2 4 4 2 b b b b . . 
+                    . b d d d d 2 4 4 2 d d d d b . 
+                    b d d b b b 2 4 4 2 b b b d d b 
+                    b d d b b b b b b b b b b d d b 
+                    b b d 1 1 3 1 1 d 1 d 1 1 d b b 
+                    . . b b d d 1 1 3 d d 1 b b . . 
+                    . . 2 2 4 4 4 4 4 4 4 4 2 2 . . 
+                    . . . 2 2 4 4 4 4 4 2 2 2 . . . 
+                    `, SpriteKind.Projectile)
+                grid.place(boomsprite, grid.getLocation(cursor))
+            } else {
+                boomsprite = sprites.create(img`
+                    . . . . . . . . b b . . . . . . 
+                    . . . . . . . b 9 1 b . . . . . 
+                    . . b b . . . b 9 9 b . . . . . 
+                    . b 9 1 b . . b b b . . b b b . 
+                    . b 3 9 b . b b b b . b 9 9 1 b 
+                    . b b b b b 9 9 1 1 b b 3 9 9 b 
+                    . . . . b 9 d 9 1 1 b b b b b . 
+                    . . . . b 5 3 9 9 9 b . . . . . 
+                    . . b b b 5 3 3 d 9 b . . . . . 
+                    . b 5 1 b b 5 5 9 b b b b . . . 
+                    . b 5 5 b b b b b b 3 9 9 3 . . 
+                    . b b b b b b b . b 9 1 1 9 b . 
+                    . . . b 5 5 1 b . b 9 1 1 9 b . 
+                    . . . b 5 5 5 b . b 3 9 9 3 b . 
+                    . . . . b b b . . . b b b b . . 
+                    . . . . . . . . . . . . . . . . 
+                    `, SpriteKind.Projectile)
+                grid.place(boomsprite, grid.getLocation(cursor))
+            }
+        }
+    }
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (moveBoatFlag == 3) {
-        cursor.setFlag(SpriteFlag.AutoDestroy, false)
+        ishitOrmiss(boatSpriteArrayP2)
     } else {
         currentBoat += 1
         grid.place(cursor, tiles.getTileLocation(0, 0))
@@ -309,6 +356,7 @@ function isOverlapping (boatSpriteArrayPX: Sprite[][]) {
     return 0
 }
 let iterator = 0
+let boomsprite: Sprite = null
 let boatRotateArrayP2: string[] = []
 let boatSpriteArrayP2: Sprite[][] = []
 let shadowCursor: Sprite = null
